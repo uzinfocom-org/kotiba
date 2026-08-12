3cc61357064afd86fbd9f2881c6f12c5b0c1c717
xfeusw <khamrakulovkamron@gmail.com>
2026-08-12 06:01:51 +0000

    Setup new formatter for Nix (#16)
    Setted up new formatter (`nixfmt`) for Nix via `Treefmt`
    
    Reviewed-on: https://git.oss.uzinfocom.uz/lambdajon/kotiba/pulls/16

--- default.nix
+++ default.nix
@@ -1,52 +1,24 @@
-{
-  dream2nix,
-  config,
-  lib,
-  pkgs,
-  ...
-}: {
-  imports = [
-    dream2nix.modules.dream2nix.WIP-haskell-cabal
-  ];
+{ dream2nix, config, lib, pkgs, ... }: {
+  imports = [ dream2nix.modules.dream2nix.WIP-haskell-cabal ];
 
   name = "kotiba";
   version = "0.1.0.0";
 
-  deps = {nixpkgs, ...}: {
+  deps = { nixpkgs, ... }: {
     haskell-compiler = nixpkgs.haskell.compiler.ghc912;
-    inherit
-      (nixpkgs)
-      bzip2
-      gnupg
-      libpq
-      libzip
-      pkg-config
-      zlib
-      git
-      ;
+    inherit (nixpkgs) bzip2 gnupg libpq libzip pkg-config zlib git;
   };
 
   mkDerivation.src = lib.cleanSourceWith {
     src = lib.cleanSource ./.;
-    filter = name: type: let
-      baseName = baseNameOf (toString name);
-    in
-      !(lib.hasSuffix ".nix" baseName);
+    filter = name: type:
+      let baseName = baseNameOf (toString name);
+      in !(lib.hasSuffix ".nix" baseName);
   };
 
   mkDerivation.buildInputs =
-    (with config.deps; [
-      zlib
-      bzip2
-      bzip2.dev
-      libzip
-      libpq
-    ])
-    ++ (with pkgs; [
-      git
-    ]);
+    (with config.deps; [ zlib bzip2 bzip2.dev libzip libpq ])
+    ++ (with pkgs; [ git ]);
 
-  mkDerivation.nativeBuildInputs = [
-    config.deps.pkg-config
-  ];
+  mkDerivation.nativeBuildInputs = [ config.deps.pkg-config ];
 }
--- flake.nix
+++ flake.nix
@@ -8,114 +8,108 @@
     git-hooks.url = "github:cachix/git-hooks.nix";
   };
 
-  outputs = {
-    self,
-    dream2nix,
-    nixpkgs,
-    git-hooks,
-  }: let
-    systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin"];
-    eachSystem = nixpkgs.lib.genAttrs systems;
-  in {
-    packages = eachSystem (system: {
-      default = dream2nix.lib.evalModules {
-        packageSets.nixpkgs = nixpkgs.legacyPackages.${system};
-        modules = [
-          ./default.nix
-          {
-            paths.projectRoot = ./.;
-            paths.projectRootFile = "flake.nix";
-            paths.package = ./.;
-          }
-        ];
-      };
-    });
+  outputs = { self, dream2nix, nixpkgs, git-hooks, }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ];
+      eachSystem = nixpkgs.lib.genAttrs systems;
+    in {
+      packages = eachSystem (system: {
+        default = dream2nix.lib.evalModules {
+          packageSets.nixpkgs = nixpkgs.legacyPackages.${system};
+          modules = [
+            ./default.nix
+            {
+              paths.projectRoot = ./.;
+              paths.projectRootFile = "flake.nix";
+              paths.package = ./.;
+            }
+          ];
+        };
+      });
 
-    checks = eachSystem (
-      system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        pre-commit = git-hooks.lib.${system}.run {
-          src = ./.;
+      checks = eachSystem (system:
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          pre-commit = git-hooks.lib.${system}.run {
+            src = ./.;
 
-          hooks = {
-            treefmt = {
-              enable = true;
-              package = pkgs.treefmt;
+            hooks = {
+              treefmt = {
+                enable = true;
+                package = pkgs.treefmt;
+              };
             };
           };
-        };
-      }
-    );
+        });
 
-    devShells = eachSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-      # hp = pkgs.haskell.packages.ghc912;
-      hlib = pkgs.haskell.lib;
-      hp = pkgs.haskell.packages."ghc912".override {
-        overrides = self: super: {
-          brick = hlib.dontCheck (hlib.doJailbreak super.brick);
-          # cabal-install = hlib.dontCheck (hlib.doJailbreak super.cabal-install);
-        };
-      };
-    in {
-      default = pkgs.mkShell {
-        nativeBuildInputs =
-          [
-            pkgs.cabal-install
-            hp.ghc
-            hp.haskell-language-server
-            hp.fourmolu
-            hp.hlint
-            hp.ghcid
-            hp.implicit-hie
-            pkgs.haskellPackages.cabal-fmt
-            pkgs.pkg-config
-            pkgs.zlib
-            pkgs.zlib.dev
-            pkgs.bzip2
-            pkgs.bzip2.dev
-            pkgs.libzip
-            pkgs.libpq
-            pkgs.libpq.dev
+      devShells = eachSystem (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          # hp = pkgs.haskell.packages.ghc912;
+          hlib = pkgs.haskell.lib;
+          hp = pkgs.haskell.packages."ghc912".override {
+            overrides = self: super: {
+              brick = hlib.dontCheck (hlib.doJailbreak super.brick);
+              # cabal-install = hlib.dontCheck (hlib.doJailbreak super.cabal-install);
+            };
+          };
+        in {
+          default = pkgs.mkShell {
+            nativeBuildInputs = [
+              pkgs.cabal-install
+              hp.ghc
+              hp.haskell-language-server
+              hp.fourmolu
+              hp.hlint
+              hp.ghcid
+              hp.implicit-hie
+              pkgs.haskellPackages.cabal-fmt
+              pkgs.pkg-config
+              pkgs.zlib
+              pkgs.zlib.dev
+              pkgs.bzip2
+              pkgs.bzip2.dev
+              pkgs.libzip
+              pkgs.libpq
+              pkgs.libpq.dev
 
-            pkgs.nixd
-            pkgs.statix
-            pkgs.deadnix
-            pkgs.treefmt
-            pkgs.alejandra
+              pkgs.nixd
+              pkgs.statix
+              pkgs.deadnix
+              pkgs.treefmt
+              pkgs.nixfmt
 
-            pkgs.jq
-            pkgs.just
-          ]
-          ++ self.checks.${system}.pre-commit.enabledPackages;
+              pkgs.jq
+              pkgs.just
+            ] ++ self.checks.${system}.pre-commit.enabledPackages;
 
-        shellHook = ''
-          echo "Welcome to kotiba dev shell"
+            shellHook = ''
+              echo "Welcome to kotiba dev shell"
 
-          ${self.checks.${system}.pre-commit.shellHook}
+              ${self.checks.${system}.pre-commit.shellHook}
 
-          export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.postgresql}/lib
-          export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.libzip}/lib
-          export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.bzip2}/lib
-          export LIBRARY_PATH=$LIBRARY_PATH:${pkgs.bzip2}/lib
-          export NIX_LDFLAGS="$NIX_LDFLAGS -L${pkgs.bzip2}/lib"
-        '';
+              export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.postgresql}/lib
+              export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.libzip}/lib
+              export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.bzip2}/lib
+              export LIBRARY_PATH=$LIBRARY_PATH:${pkgs.bzip2}/lib
+              export NIX_LDFLAGS="$NIX_LDFLAGS -L${pkgs.bzip2}/lib"
+            '';
 
-        NIX_CONFIG = "extra-experimental-features = nix-command flakes";
-      };
-    });
-    apps = eachSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-      refresh = self.packages.${system}.default.config.lock.refresh;
-    in {
-      update-lock = {
-        type = "app";
-        program = "${pkgs.writeShellScript "update-lock" ''
-          export PATH="${pkgs.git}/bin:${pkgs.cabal-install}/bin:$PATH"
-          exec ${nixpkgs.lib.getExe refresh}
-        ''}";
-      };
-    });
-  };
+            NIX_CONFIG = "extra-experimental-features = nix-command flakes";
+          };
+        });
+      apps = eachSystem (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          refresh = self.packages.${system}.default.config.lock.refresh;
+        in {
+          update-lock = {
+            type = "app";
+            program = "${pkgs.writeShellScript "update-lock" ''
+              export PATH="${pkgs.git}/bin:${pkgs.cabal-install}/bin:$PATH"
+              exec ${nixpkgs.lib.getExe refresh}
+            ''}";
+          };
+        });
+    };
 }
